e8630127186aae34c3a29ed5771d39a6c9861356
Julian Hurtado <ealejandroh.julian@gmail.com>
2020-06-13 09:18:27 -0500

    proseso

--- JavaScripT/iniciacion.js
+++ JavaScripT/iniciacion.js
@@ -1,3 +1,12 @@
+function  ejecuta () {
+    var z=document.getElementsByClassName("importante") [0].onclick=saludo;
+    // for (var i=0;i<3;i++){
+    //  document.getElementsByTagName("p") [i].onclick=saludo;
+    }
+    /* document.getElementsById("importante").onclick=saludo;*/
+}
 function saludo () {
     alert("hola amigo");
 }
+
+window.onload=ejecuta;
